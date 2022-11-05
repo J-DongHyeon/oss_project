d18dca8e4fb3c5ac31d8104d54676b1fe2523bb2
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2022-11-06 00:25:16 +0900

    create; *.html

--- index.js
+++ index.js
@@ -8,3 +8,40 @@ app.listen(8080, function() {
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/running_etc1', function(req, res) {
+    res.sendFile(__dirname + '/running_etc1.html')
+})
+
+app.get('/running_etc2', function(req, res) {
+    res.sendFile(__dirname + '/running_etc2.html')
+})
+
+app.get('/running_etc3', function(req, res) {
+    res.sendFile(__dirname + '/running_etc3.html')
+})
+
+app.get('/cycling_etc1', function(req, res) {
+    res.sendFile(__dirname + '/cycling_etc1.html')
+})
+
+app.get('/cycling_etc2', function(req, res) {
+    res.sendFile(__dirname + '/cycling_etc2.html')
+})
+
+app.get('/cycling_etc3', function(req, res) {
+    res.sendFile(__dirname + '/cycling_etc3.html')
+})
+
+app.get('/skiing_etc1', function(req, res) {
+    res.sendFile(__dirname + '/skiing_etc1.html')
+})
+
+app.get('/skiing_etc2', function(req, res) {
+    res.sendFile(__dirname + '/skiing_etc2.html')
+})
+
+app.get('/skiing_etc3', function(req, res) {
+    res.sendFile(__dirname + '/skiing_etc3.html')
+})
+
